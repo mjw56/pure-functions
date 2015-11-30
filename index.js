aa0ea76be2d2821e33c1344e57c8a2d5c5dc922d
Mike Wilcox <mwilcox56@gmail.com>
2015-11-30 00:05:17 -0500

    add eval and curry example

--- index.js
+++ index.js
@@ -8,3 +8,26 @@ function square(n) {
 console.log(
   expect(square(2)).toEqual(4)
 );
+
+// curry functions
+function sum(x) {
+  return function(y) {
+    return x + y;
+  };
+}
+
+var addTwenty = sum(20);
+
+console.log(
+  expect(addTwenty(10)).toEqual(30)
+);
+
+// cheat lexical scope
+function multiplyCheat(a) {
+  eval(a);
+  return b * 2;
+}
+
+console.log(
+  expect(multiplyCheat('var b = 2;')).toEqual(4)
+);
